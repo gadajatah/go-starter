669685b2ab52d3bbe2a3ad23830d88d3986574d1
Jhon Vebrianto Girsang <jvg.inq@gmail.com>
2023-07-25 15:20:08 +0700

    _add url for test

--- server.go
+++ server.go
@@ -12,5 +12,13 @@ func main() {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
 
+	e.GET("/about", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, This is about page!")
+	})
+
+	e.GET("/contact", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, This is contact page!")
+	})
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
